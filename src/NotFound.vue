62274edd9f2de7a9a2991faf78d9d3af64d28557
Samith Vinura <samithvinurakck119@gmail.com>
2025-07-22 10:15:53 +0530

    add not found page design

--- src/NotFound.vue
+++ src/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1></h1>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe checkout all our
+        <router-link to="/coaches">Coaches</router-link>
+      </p></base-card
+    >
+  </section>
 </template>
