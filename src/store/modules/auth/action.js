7a620c645f53ae707b1add30c4251f72906c7433
Samith Vinura <samithvinurakck119@gmail.com>
2025-07-24 11:13:44 +0530

    adding login action flow

--- src/store/modules/auth/action.js
+++ src/store/modules/auth/action.js
@@ -1,5 +1,32 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDwSQsqTYsMCJHEX_Nmi7p0c3AZ-5uXtTo',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to send authenticate!'
+      );
+      throw error;
+    }
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
   async signup(context, payload) {
     const response = await fetch(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDwSQsqTYsMCJHEX_Nmi7p0c3AZ-5uXtTo',
