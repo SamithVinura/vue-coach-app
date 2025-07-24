d7782c85142a844aa9a9dbb97f57c691545c622d
Samith Vinura <samithvinurakck119@gmail.com>
2025-07-24 10:12:53 +0530

    preparing vuex for auth module

---
+++ src/store/modules/auth/index.js
@@ -0,0 +1,14 @@
+import getters from './getters';
+import mutations from './mutation';
+import actions from './action';
+
+export default {
+  state() {
+    return {
+      userId: 'c3',
+    };
+  },
+  mutations,
+  actions,
+  getters,
+};
