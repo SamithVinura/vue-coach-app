862ee2a5e16648bd0fd4065e4b67ac03a0e0733d
Samith Vinura <samithvinurakck119@gmail.com>
2025-07-25 08:05:46 +0530

    adding auto login

--- src/store/modules/auth/action.js
+++ src/store/modules/auth/action.js
@@ -1,59 +1,9 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDwSQsqTYsMCJHEX_Nmi7p0c3AZ-5uXtTo',
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-
-    const responseData = await response.json();
-    if (!response.ok) {
-      const error = new Error(
-        responseData.message || 'Failed to send authenticate!'
-      );
-      throw error;
-    }
-
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
-    });
+    return context.dispatch('auth', { ...payload, mode: 'login' });
   },
   async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDwSQsqTYsMCJHEX_Nmi7p0c3AZ-5uXtTo',
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-
-    const responseData = await response.json();
-    if (!response.ok) {
-      const error = new Error(
-        responseData.message || 'Failed to send authenticate!'
-      );
-      throw error;
-    }
-
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
-    });
+    return context.dispatch('auth', { ...payload, mode: 'signup' });
   },
   logOut(context) {
     context.commit('setUser', {
@@ -61,5 +11,52 @@ export default {
       userId: null,
       tokenExpiration: null,
     });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDwSQsqTYsMCJHEX_Nmi7p0c3AZ-5uXtTo';
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDwSQsqTYsMCJHEX_Nmi7p0c3AZ-5uXtTo';
+    }
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
+
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to send authenticate!'
+      );
+      throw error;
+    }
+
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null,
+      });
+    }
   },
 };
